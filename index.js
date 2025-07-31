a698bf810a60afe3ee542e96d14fce08aecd4f8b
Tiger121612 <159625046+Tiger121612@users.noreply.github.com>
2025-07-31 16:27:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const checkRoute = require('./api/check');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use('/api/check', checkRoute);
+
+app.get('/', (req, res) => {
+  res.send('Mondrop backend is live');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
