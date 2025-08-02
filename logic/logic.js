fc2234be8c7e96bcd54dd0ca7583ec4e16036473
Tiger121612 <159625046+Tiger121612@users.noreply.github.com>
2025-08-02 07:16:27 +0530

    Create logic.js

---
+++ logic/logic.js
@@ -0,0 +1,4 @@
+export async function checkEligibility(address) {
+  // Dummy test logic — you can replace this
+  return address.toLowerCase().startsWith('0x4'); // example: all starting with 0x4 eligible
+}
