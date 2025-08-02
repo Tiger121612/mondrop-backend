7e39b2dd92d31e2bf5ac75e29d41f066db5fd7fb
Tiger121612 <159625046+Tiger121612@users.noreply.github.com>
2025-08-02 10:05:22 +0530

    Delete logic directory

--- logic/logic.js
+++
@@ -1,4 +0,0 @@
-export async function checkEligibility(address) {
-  // Dummy test logic — you can replace this
-  return address.toLowerCase().startsWith('0x4'); // example: all starting with 0x4 eligible
-}
